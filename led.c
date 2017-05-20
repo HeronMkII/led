fe4cb49f1c520b052e049c683652f10bdeee77ad
Siddharth Mahendraker <siddharth.mahen@gmail.com>
2017-05-20 14:55:53 -0400

    Added chip select toggle

--- led.c
+++ led.c
@@ -8,20 +8,29 @@
 // CLK/SCK - pin 28 - PB7
 // MISO - pin 8 - PB0
 // MOSI - pin - PB1
-// CS - pin 18 - PC5
+// old CS - pin 18 - PC5
+// CS - PB2
 
 /*
  * Before SPI is enabled, the power reduction register must have the SPI bit (the third bit)
  * set to 0.
  */
 
+void set_cs_low() {
+    PORTB &= ~(_BV(PB2));
+}
+
+void set_cs_high() {
+    PORTB |= _BV(PB2);
+}
+
 void init_spi(void) {
     // PRR = _BV(2); // because PRRSPI is bit 2 (counting from 0)
     // set SCK and MOSI as output pins
-    DDRB |= _BV(PB7) | _BV(PB1);
-    //PORTB &= ~(_BV(PB7)); // drive CLK low
     // set CS as an output pin
-    DDRC |= _BV(PC5);
+    DDRB |= _BV(PB7) | _BV(PB2) | _BV(PB1);
+    //PORTB &= ~(_BV(PB7)); // drive CLK low
+    //DDRC |= _BV(PC5);
     SPCR |= _BV(6) | _BV(4) | _BV(1);
     // enable SPI, set mode to master, set SCK freq to f_io/64
 }
@@ -32,7 +41,6 @@ char send_spi(char cmd) {
     // thankfully, the thermometer we're using is compatible with the default
     // data mode (SPI mode 0).
     //
-    PORTC &= ~(_BV(PC5));
     SPDR = cmd;
     while (!(SPSR & _BV(7)));
 
@@ -80,27 +88,12 @@ int main(void)
     DDRB |= _BV(PB6);
 
     init_spi();
-
-    //send_spi(0x1E); // RESET CMD
+    set_cs_low();
+    //send_spi(0x3D); // nop - returns status byte
+    //send_spi(0x35); // enable TX
+    set_cs_high();
     //delay(3); // wait for RESET
 
-    char a = send_spi(0x3D); // NOP cmd to get status byte
-    readout(a);
-    /*
-    char a = send_spi(0xAE); // PROM READ C5
-    char b = send_spi(0xFF); // dummy to get rest of C5
-    char c = send_spi(0xFF); // read more
-
-    readout(a);
-    readout(b);
-    readout(c);
-    */
-
-    /*
-    while(1) {
-        PORTC ^= _BV(PC0);
-        delay(10);
-        _NOP();
-    }
-    */
+    //char a = send_spi(0x3D); // NOP cmd to get status byte
+    //readout(a);
 }
